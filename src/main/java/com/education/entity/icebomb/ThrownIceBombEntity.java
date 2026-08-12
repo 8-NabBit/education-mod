31bb30a7b8b477b22a4f7f3bd6d5718c7e11fbc9
8-NabBit <tijnbuijsman07@gmail.com>
2026-08-12 20:12:30 +0200

    freezeWaterAround ice_bomb

--- src/main/java/com/education/entity/icebomb/ThrownIceBombEntity.java
+++ src/main/java/com/education/entity/icebomb/ThrownIceBombEntity.java
@@ -1,21 +1,18 @@
 package com.education.entity.icebomb;
 
 import com.education.entity.ModEntities;
-import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrowableItemProjectile;
+import com.education.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import com.education.item.ModItems;
+import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ThrownIceBombEntity extends ThrowableItemProjectile {
     public ThrownIceBombEntity(EntityType<? extends ThrowableItemProjectile> type, Level level) {
@@ -33,26 +30,47 @@ public class ThrownIceBombEntity extends ThrowableItemProjectile {
 
     protected void onHitEntity(final EntityHitResult hitResult) {
         super.onHitEntity(hitResult);
+        this.discard();
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        Level level = this.level();
+        if (level.isClientSide()) {
+            return;
+        }
+
+        if (this.isInLava() || level.getFluidState(this.blockPosition()).is(FluidTags.LAVA)) {
+            this.discard();
+        }
+
+        if (this.isInWater() || level.getFluidState(this.blockPosition()).is(FluidTags.WATER)) {
+            freezeWaterAround(this.blockPosition());
+            this.discard();
+        }
     }
 
     protected void onHitBlock(final BlockHitResult hitResult) {
         super.onHitBlock(hitResult);
 
+        freezeWaterAround(hitResult.getBlockPos());
+        this.discard();
+    }
+
+    private void freezeWaterAround(BlockPos center) {
         Level level = this.level();
-        List<BlockPos> positions = new ArrayList<BlockPos>();
+
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
                 for (int z = -1; z <= 1; z++) {
-                    Block block = level.getBlockState(hitResult.getBlockPos()).getBlock();
-                    if (block != null && block != Blocks.AIR) {
-                        positions.add(hitResult.getBlockPos().offset(x, y, z));
+                    BlockPos pos = center.offset(x, y, z);
+                    if (level.getFluidState(pos).is(FluidTags.WATER)) {
+                        level.setBlockAndUpdate(pos, Blocks.ICE.defaultBlockState());
                     }
                 }
             }
         }
-        for (BlockPos pos : positions) {
-            level.setBlockAndUpdate(pos, Blocks.ICE.defaultBlockState());
-        }
-        this.discard();
     }
 }
