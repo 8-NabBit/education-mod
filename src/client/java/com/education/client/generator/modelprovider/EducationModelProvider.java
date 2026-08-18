1a067221ec6fbdf4f6c0a9f4136e1842adc24b7d
8-NabBit <tijnbuijsman07@gmail.com>
2026-08-18 18:40:43 +0200

    ModelProvider class

---
+++ src/client/java/com/education/client/generator/modelprovider/EducationModelProvider.java
@@ -0,0 +1,26 @@
+package com.education.client.generator.modelprovider;
+
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+
+public class EducationModelProvider extends FabricModelProvider {
+	public EducationModelProvider(FabricPackOutput output) {
+		super(output);
+	}
+
+	@Override
+	public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+		
+	}
+
+	@Override
+	public void generateItemModels(ItemModelGenerators itemModelGenerator) {
+	}
+
+	@Override
+	public String getName() {
+		return "EducationModelProvider";
+	}
+}
