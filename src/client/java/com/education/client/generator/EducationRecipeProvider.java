688ad380cd4389f02fb4fdfd6d1a60d2083f2e71
TheOnlineOnion <jelle.zijlstra.2008@gmail.com>
2026-08-24 22:40:15 +0200

    fixed RecipeProvider issue

---
+++ src/client/java/com/education/client/generator/EducationRecipeProvider.java
@@ -0,0 +1,31 @@
+package com.education.client.generator;
+
+import java.util.concurrent.CompletableFuture;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.RecipeOutput;
+
+public class EducationRecipeProvider extends FabricRecipeProvider {
+    public EducationRecipeProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeProvider createRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+        return new RecipeProvider(registries, output) {
+            @Override
+            public void buildRecipes() {
+                
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "EducationRecipeProvider";
+    }
+    
+}
